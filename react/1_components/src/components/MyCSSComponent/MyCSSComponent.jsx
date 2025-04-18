c78a8991b793fc5ff4d234e8700bbe3354fb8b7b
Gabrielly <gabriellysilva.oli01@gmail.com>
2025-04-18 14:21:15 -0300

    Seção 01 | Fundamentos | Aula 11 - CSS linkado

--- react/1_components/src/components/MyCSSComponent/MyCSSComponent.jsx
+++ react/1_components/src/components/MyCSSComponent/MyCSSComponent.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
 
+// Importando o arquivo de estilos CSS para o componente
+import './MyCSSComponent.css'
+
+// Quando usamos CSS externo assim, pode rolar o risco de estilos vazarem
+// e afetarem outros componentes que usam a mesma tag, tipo um <h1> por exemplo.
+
 const MyCSSComponent = () => {
 
-    // Construímos um objeto com as propriedades CSS 
+    // Criando um objeto com estilos inline (aplicados diretamente no elemento)
     const cssInline = {
-        backgroundColor: "#000000",
-        color: "#f00000",
-        fontSize: "1.25rem",
-        borderRadius: "8px",
-        padding: "20px"
+        backgroundColor: "#000000", // fundo preto
+        color: "#f00000",           // texto vermelho
+        fontSize: "1.25rem",        // tamanho da fonte
+        borderRadius: "8px",        // bordas arredondadas
+        padding: "20px"             // espaço interno
     }
 
     return (
         <>
-            {/* Passamos o objeto 'cssInline' como referência para a propriedade 'style'  */}
+            <h1>Meu título</h1>
+
+            {/* Aplicando os estilos diretamente no parágrafo com o objeto 'cssInline' */}
             <p style={cssInline}>Meu Parágrafo</p>
         </>
     )
