43a7f0e4c0f1af6d80cd1c508672f9267fd56cbf
Gabrielly <gabriellysilva.oli01@gmail.com>
2025-04-18 12:40:12 -0300

    Seção 01 | Fundamentos | Aula 07

---
+++ react/1_components/src/components/ComponentExample.jsx
@@ -0,0 +1,21 @@
+/* ATALHOS */
+
+// rfce
+import React from 'react'
+
+const ComponentExample = () => {
+  return (
+    <div>ComponentExample</div>
+  )
+}
+
+export default ComponentExample
+
+// raf
+import React from 'react'
+
+export const ComponentExample = () => {
+  return (
+    <div>ComponentExample</div>
+  )
+}
