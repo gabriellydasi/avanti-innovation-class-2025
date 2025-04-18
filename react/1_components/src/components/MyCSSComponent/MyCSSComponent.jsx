c8080fe2de91fbcca3a542b3be2815ba30603169
Gabrielly <gabriellysilva.oli01@gmail.com>
2025-04-18 14:34:03 -0300

    Seção 01 | Fundamentos | Aula 12 | CSS modules

--- react/1_components/src/components/MyCSSComponent/MyCSSComponent.jsx
+++ react/1_components/src/components/MyCSSComponent/MyCSSComponent.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 // Importando o arquivo de estilos CSS para o componente
-import './MyCSSComponent.css'
+// import './MyCSSComponent.module.css'
+
+// Importando os estilos como um objeto usando CSS Modules
+// Cada classe definida no CSS vira uma propriedade de 'styles'
+import styles from './MyCSSComponent.module.css'
 
 // Quando usamos CSS externo assim, pode rolar o risco de estilos vazarem
 // e afetarem outros componentes que usam a mesma tag, tipo um <h1> por exemplo.
@@ -19,7 +23,15 @@ const MyCSSComponent = () => {
 
     return (
         <>
-            <h1>Meu título</h1>
+            {/* <h1>Meu título</h1> */}
+
+            <h1 className={styles.title}>Meu título</h1>
+            {/* 
+                Usando a classe 'title' definida no CSS Module.
+                Aqui, styles.title é como acessar a propriedade 'title' do objeto styles.
+                Isso garante que o estilo fique escopado só pra esse componente,
+                evitando que outros <h1> em outros arquivos sejam afetados 
+            */}
 
             {/* Aplicando os estilos diretamente no parágrafo com o objeto 'cssInline' */}
             <p style={cssInline}>Meu Parágrafo</p>
