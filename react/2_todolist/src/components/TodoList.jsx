e64e7a5129f18114117318abff646feeba4c744e
Gabrielly <gabriellysilva.oli01@gmail.com>
2025-04-19 21:03:33 -0300

    Seção 02 | Hooks useState e useEffects | Aula 19 - Projeto todolist - estrutura

--- react/2_todolist/src/components/TodoList.jsx
+++ react/2_todolist/src/components/TodoList.jsx
@@ -4,7 +4,20 @@ import styles from './TodoList.module.css'
 
 const TodoList = () => {
   return (
-    <div>TodoList</div>
+    <div className={styles.container}>
+        <h2 className={styles.title}>Lista de Tarefas</h2>
+        <div className={styles.inputContainer}>
+            <input 
+                type='text'
+                placeholder='Adicione uma tarefa'
+                className={styles.input}
+            />
+            <button className={styles.button}>Adicionar</button>
+        </div>
+        <ul className={styles.taskList}>
+            <li className={styles.taskItem}>Tarefa</li>
+        </ul>
+    </div>
   )
 }
 
