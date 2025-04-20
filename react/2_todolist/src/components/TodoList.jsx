8870dca7197115bf29f69ae38d4356470cf986fa
Gabrielly <gabriellysilva.oli01@gmail.com>
2025-04-19 21:08:40 -0300

    Seção 02 | Hooks useState e useEffects | Aula 20 - Projeto todolist - controlando estado do input

--- react/2_todolist/src/components/TodoList.jsx
+++ react/2_todolist/src/components/TodoList.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
+import { useState } from 'react'
 
 import styles from './TodoList.module.css' 
 
 const TodoList = () => {
-  return (
-    <div className={styles.container}>
-        <h2 className={styles.title}>Lista de Tarefas</h2>
-        <div className={styles.inputContainer}>
-            <input 
-                type='text'
-                placeholder='Adicione uma tarefa'
-                className={styles.input}
-            />
-            <button className={styles.button}>Adicionar</button>
+    const [task, setTask] = useState("")
+    // testando
+    console.log("task: ", task)
+
+    return (
+        <div className={styles.container}>
+            <h2 className={styles.title}>Lista de Tarefas</h2>
+            <div className={styles.inputContainer}>
+                <input 
+                    type='text'
+                    placeholder='Adicione uma tarefa'
+                    className={styles.input}
+                    value={task}
+                    onChange={(e) => setTask(e.target.value)}
+                />
+                <button className={styles.button}>Adicionar</button>
+            </div>
+            <ul className={styles.taskList}>
+                <li className={styles.taskItem}>Tarefa</li>
+            </ul>
         </div>
-        <ul className={styles.taskList}>
-            <li className={styles.taskItem}>Tarefa</li>
-        </ul>
-    </div>
-  )
+    )
 }
 
 export default TodoList
