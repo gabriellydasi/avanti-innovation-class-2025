6a84baaaa4643b8ecbec587cfe0a07fde1a530aa
Gabrielly <gabriellysilva.oli01@gmail.com>
2025-04-22 14:14:24 -0300

    Seção 04 | Context API | Aula 37 - Desenvolvendo toggle theme - Context API - Consumindo o contexto

--- react/4_themecontextapi/src/App.jsx
+++ react/4_themecontextapi/src/App.jsx
@@ -1,10 +1,27 @@
+import React from 'react'
+
+import { ThemeProvider, useTheme } from './components/ThemeContext'
+
 import './App.css'
+import Header from './components/Header'
+import Content from './components/Content'
+
+const ThemeApp = () => {
+  const {theme} = useTheme()
+
+  return (
+    <div className={`app ${theme}`}>
+      <Header />
+      <Content />
+    </div>
+  )
+}
 
 function App() {
   return (
-    <>
-      <h1>Context API</h1>
-    </>
+    <ThemeProvider>
+      <ThemeApp />
+    </ThemeProvider>
   )
 }
 
